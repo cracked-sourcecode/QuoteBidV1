c651cd2c356d62f888298493ea21931d50a6fdbc
Ben Deveran <bendeveran@Bens-MBP.lan>
2025-05-18 23:42:43 -0400

    Add Ruby dependencies to replit.nix

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+  deps = [
+    pkgs.nodejs-20_x
+    pkgs.nodePackages.typescript-language-server
+    pkgs.yarn
+    pkgs.replitPackages.jest
+    pkgs.ruby
+    pkgs.rubyPackages.parser
+  ];
+} 
